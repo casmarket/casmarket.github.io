55dfb5429acad15d4bf9d18caa997ad08f61bd61
100の人 <100@pokemori.jp>
2021-10-23 16:29:40 +0900

    [item-list] 全体的なリファクタリング

--- docs/scripts/item-list.js
+++ docs/scripts/item-list.js
@@ -65,7 +65,7 @@ customElements.define('item-list', class extends LitElement {
 		.name {
 			grid-area: name;
 		}
-		.badge {
+		.badge img {
 			height: 1.5em;
 			margin-right: 0.2em;
 		}
@@ -97,132 +97,56 @@ customElements.define('item-list', class extends LitElement {
 		super();
 		this.catalogue = [ ];
 		(async () => {
-			this.catalogue = await (await fetch('../6/catalogue.json')).json();
-			console.log(this.catalogue);
+			this.catalogue = await (await fetch('catalogue.json')).json();
 		})();
 	}
 
 
 	render()
 	{
-		return html`<section id="frontier">
-		<h1>ルーム会場「オリンピア」</h1>
-		<section>
-			<h1><img src="images/theme-logp1.png" alt="機能" /></h1>
-			<a target="_blank" href="../images/room1-map.png"><img src="../images/room1-map.png" /></a>
+		return [
+			[ 'theme1', 'テーマ会場', '1. 機能' ],
+			[ 'theme2', 'テーマ会場', '2. 機能' ],
+			[ 'simple', 'シンプル会場', null ],
+		].map(([ classId, heading, subHeading ]) => html`<section>
+			<h1><img src="../images/heading-${classId}.png" alt="${heading} ${subHeading}" /></h1>
+			<a target="_blank" href="images/map-${classId}.png"><img src="images/map-${classId}.png" /></a>
 			<table>
 				<thead>
 					<tr>
 						<th>ID</th>
 						<th>アイコン</th>
-						<th>名前</th>
+						<th>バッヂ</th>
+						<th>出展者</th>
+						<th>作品</th>
+						<th>Twitter</th>
+						<th>THE SEED ONLINE</th>
 					</tr>
 				</thead>
-				<tbody>
-					${this.catalogue && this.catalogue.filter(participant => participant.classId.includes('theme1'))
-					.map(participant => html`<tr>
-					<th class="id">${participant.id}</th>
-					<td class="icon"><img src="${participant.exhibitor.icon}" /></td>
-					<td class="name">
-						<img src="../images/${participant.badge}.png" alt="[${participant.badge}]" class="badge" />
-						<a rel="external" target="_blank" href="${participant.exhibitor.twitterURL}">
-						${participant.exhibitor.name}</a>
+				<tbody>${this.catalogue.filter(item => item.classId === classId).map(item => html`<tr>
+					<th class="id">${item.id}</th>
+					<td class="icon"><img src="${item.exhibitor.icon}" /></td>
+					<td class="badge">
+						${item.badge && html`<img src="../images/${item.badge}.png" alt="${item.badge}" />`}
 					</td>
+					<td class="name">
+						<a rel="external" target="_blank" href="${item.exhibitor.twitterURL}">${item.exhibitor.name}</a>
+					</td>
+					<td class="item">${ classId === 'simple'
+						? html`<a rel="external" target="_blank" href="${item.item.url}">${item.item.title} </a>`
+						: null}</td>
 					<td class="twitter">
-						<a rel="external" target="_blank" href="${participant.exhibitor.twitterURL}">
+						<a rel="external" target="_blank" href="${item.exhibitor.twitterURL}">
 							<img src="../images/twitter-logo.png" alt="Twitter" />
 						</a>
 					</td>
 					<td class="seed">
-						<a rel="external" target="_blank" href="${participant.exhibitor.tsoURL}">
+						<a rel="external" target="_blank" href="${item.exhibitor.tsoURL}">
 							<img src="../images/theseedonline-logo.png" alt="THE SEED ONLINE" />
 						</a>
 					</td>
-				</tr>`)}
-				</tbody>
+				</tr>`)}</tbody>
 			</table>
-		</section>
-		<section>
-			<h1><img src="images/theme-logp2.png" alt="装飾" /></h1>
-			<a target="_blank" href="images/room2-map.png"><img src="images/room2-map.png" /></a>
-			<table>
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>アイコン</th>
-						<th>名前</th>
-					</tr>
-				</thead>
-				<tbody>
-					${this.catalogue && this.catalogue.filter(participant => participant.classId.includes('theme2'))
-					.map(participant => html`<tr>
-					<th class="id">${participant.id}</th>
-					<td class="icon"><img src="${participant.exhibitor.icon}" /></td>
-					<td class="name">
-						<img src="../images/${participant.badge}.png" alt="[${participant.badge}]" class="badge" />
-						<a rel="external" target="_blank" href="${participant.exhibitor.twitterURL}">
-						${participant.exhibitor.name}</a>
-					</td>
-					<td class="twitter">
-						<a rel="external" target="_blank" href="${participant.exhibitor.twitterURL}">
-							<img src="../images/twitter-logo.png" alt="Twitter" />
-						</a>
-					</td>
-					<td class="seed">
-						<a rel="external" target="_blank" href="${participant.exhibitor.tsoURL}">
-							<img src="../images/theseedonline-logo.png" alt="THE SEED ONLINE" />
-						</a>
-					</td>
-				</tr>`)}
-				</tbody>
-			</table>
-		</section>
-	</section>
-	<section id="simple">
-		<h1>シンプル会場「神殿」</h1>
-		<section>
-		<h1><img src="images/simple-logo.png" alt="神殿" /></h1>
-		<a target="_blank" href="images/simple-map.png"><img src="images/simple-map.png" /></a>
-		<table>
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>アイコン</th>
-					<th>名前</th>
-					<th>作品1</th>
-				</tr>
-			</thead>
-			<tbody>
-			${this.catalogue && this.catalogue.filter(participant => participant.classId.includes('simple'))
-			.map(participant => html`<tr>
-			<th class="id">${participant.id}</th>
-			<td class="icon"><img src="${participant.exhibitor.icon}" /></td>
-			<td class="name">
-				<img src="../images/${participant.badge}.png" alt="[${participant.badge}]" class="badge" />
-				<a rel="external" target="_blank" href="${participant.exhibitor.twitterURL}">
-					${participant.exhibitor.name}
-				</a>
-			</td>
-			<td class="work1">
-				<a rel="external" target="_blank"
-					href="${participant.item.url}"
-					title="${participant.item.title}">
-					${participant.item.title} </a>
-			</td>
-			<td class="twitter">
-				<a rel="external" target="_blank" href="${participant.exhibitor.twitterURL}">
-					<img src="../images/twitter-logo.png" alt="Twitter" />
-				</a>
-			</td>
-			<td class="seed">
-				<a rel="external" target="_blank" href="${participant.exhibitor.tsoURL}">
-					<img src="../images/theseedonline-logo.png" alt="THE SEED ONLINE" />
-				</a>
-			</td>
-		</tr>`)}
-			</tbody>
-		</table>
-		</section>
-	</section>`;
+		</section>`);
 	}
 });
