e4b911367f71f7072812530b5861c3c2cb9ae6be
BellaPasta <BellaPasta@users.noreply.github.com>
2022-04-23 09:50:24 +0000

    deploy: 9e6642a1f9aa32a9f8a256b21d35155fb059c411

--- scripts/index.js
+++ scripts/index.js
@@ -856,8 +856,8 @@ customElements.define('item-list', class extends lit__WEBPACK_IMPORTED_MODULE_0_
 	{
 		return [
 			[ 'simple', 'シンプル会場', null ],
-			[ 'theme1', 'テーマ会場', '1. 機能' ],
-			[ 'theme2', 'テーマ会場', '2. 装飾' ],
+			[ 'theme1', 'テーマ会場', '1. リアル' ],
+			[ 'theme2', 'テーマ会場', '2. ファンタジー' ],
 			this.showPosters && [ 'poster', '広告', null ],
 		].filter(classInfo => classInfo && this.catalogue.some(item => item.classId === classInfo[0]))
 			.map(([ classId, heading, subHeading ]) => lit__WEBPACK_IMPORTED_MODULE_0__.html`<section>
