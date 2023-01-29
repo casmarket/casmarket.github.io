19aaeff8b73ea8032501113c4855dd7e78bccfbc
ベッラパスタ <90831677+BellaPasta@users.noreply.github.com>
2023-01-29 21:27:51 +0900

    ポスターサイズ調整

--- docs/scripts/item-list.js
+++ docs/scripts/item-list.js
@@ -69,8 +69,7 @@ customElements.define('item-list', class extends LitElement {
 		}
 
 		:is([href*="images/map-"], [href*="posters/"]) img {
-			height: 100%;
-			max-height: 500px;
+
 			max-width: 100%;
 		}
 
